85a365f777c68e631d1297ae4d66c9ac7f1bdab2
susmita2006 <i.susmitabiswas@gmail.com>
2020-12-18 14:57:56 +0530

    First Commit

---
+++ info.php
@@ -0,0 +1,16 @@
+<?php
+//phpinfo();
+
+?>
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script>
+    var cats=2;
+    function mycats(){
+        if(cats < 5){
+            console.log('I have ${cats} at me'); 
+        }
+        else{
+            console.log('That a lot of cats);
+        }
+    }
+    </script>
